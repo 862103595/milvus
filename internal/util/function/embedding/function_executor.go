9876e7dab884d110b09a00a76b4e7c72affd788d
xiejh <862103595@qq.com>
2026-01-15 17:37:13 +0900

    feat: integrate MOL fingerprint function into embedding pipeline
    - Register MolFingerprint function runner in function factory
    - Add MOL fingerprint embedding support in flushcommon pipeline
    - Add MOL fingerprint embedding support in querynodev2 pipeline
    - Implement BuildBinaryFieldData helper for binary vector field construction

--- internal/util/function/embedding/function_executor.go
+++ internal/util/function/embedding/function_executor.go
@@ -70,6 +70,7 @@ func createFunction(coll *schemapb.CollectionSchema, schema *schemapb.FunctionSc
 		}
 		return f, nil
 	case schemapb.FunctionType_MinHash:
+	case schemapb.FunctionType_MolFingerprint: // ignore, handled by function.NewFunctionRunner
 		return nil, nil
 	default:
 		return nil, fmt.Errorf("unknown functionRunner type %s", schema.GetType().String())
